6db9875ba4f92e7a50434df165a2bc37b417c055
damithc <1673303+damithc@users.noreply.github.com>
2025-10-31 01:53:59 +0800

    Add configs

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
 ./get-reposense.py --master
-java -jar RepoSense.jar --config ./config --since 31-10-2025T10:00  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
+java -jar RepoSense.jar --config ./config --since 31/10/2025T10:00  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
